f30dc2c22852af2525880e79efda8a2aa793c323
vasudev888 <vdk5384@gmail.com>
2024-12-16 14:37:59 +0530

    Calibration changes without button click

--- Assets/Demo/Scripts/ShowBreakDown.cs
+++ Assets/Demo/Scripts/ShowBreakDown.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ShowBreakDown : MonoBehaviour
 {
     [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject closeButton;
+
     public void EnableGameObject(bool enable)
     {
         if (canvas != null)
@@ -22,6 +24,34 @@ public class ShowBreakDown : MonoBehaviour
         if (canvas != null)
         {
             canvas.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("Target GameObject is not assigned.");
+        }
+    }
+
+
+    public void ScaleImageGameObject(bool enable)
+    {
+        if (canvas != null)
+        {
+            canvas.transform.localScale = new Vector3(1, 1, 1);
+            closeButton.SetActive(true);
+
+        }
+        else
+        {
+            Debug.LogError("Target GameObject is not assigned.");
+        }
+    }
+
+    public void DeScaleGameObject(bool enable)
+    {
+        if (canvas != null)
+        {
+            canvas.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            closeButton.SetActive(false);
         }
         else
         {
